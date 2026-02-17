e50d29246cea2bc799631347e8b78bc9fc2e637c
sandra67890 <corina.alexandra.necula@gmail.com>
2026-02-17 23:09:28 +0200

    Prepare settings for deployment

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -119,3 +119,7 @@ STATIC_URL = 'static/'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/'
+
+import os
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
